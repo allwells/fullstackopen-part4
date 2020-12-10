1a72de0b5fc06ef503849b5c6d6176ec7734b239
Allwel Onen <aleenfestus@gmail.com>
2020-12-10 01:52:40 +0100

    Modify

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -9,12 +9,25 @@ blogRouter.get("/", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-blogRouter.post("/", (request, response) => {
+blogRouter.delete("/:id", (request, response, next) => {
+  const id = request.params.id;
+
+  Blog.findByIdAndDelete(id)
+    .then((blogs) => {
+      response.json(blogs);
+    })
+    .catch((error) => next(error));
+});
+
+blogRouter.post("/", (request, response, next) => {
   const blog = new Blog(request.body);
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result);
+    })
+    .catch((error) => next(error));
 });
 
 module.exports = blogRouter;
