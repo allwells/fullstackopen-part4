5c102d9978f3cb6096e845f246b7354cec4656f0
Allwel Onen <aleenfestus@gmail.com>
2020-12-15 02:33:59 +0100

    Run tests

--- tests/node_api.test.js
+++ tests/node_api.test.js
@@ -11,16 +11,16 @@ test("Notes are returned as json", async () => {
     .expect("Content-Type", /application\/json/);
 });
 
-test("There are two blogs", async () => {
+test("There are five blogs", async () => {
   const response = await api.get("/api/blogs");
 
-  expect(response.body).toHaveLength(7);
+  expect(response.body).toHaveLength(4);
 });
 
-test("The first blog is about HTTP methods", async () => {
+test("The first blog is about Github", async () => {
   const response = await api.get("/api/blogs");
 
-  expect(response.body[0].content).toBe("allwells");
+  expect(response.body[1].title).toBe("Github");
 });
 
 afterAll(() => {
